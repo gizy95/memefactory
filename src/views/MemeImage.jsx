b72c901a90348dda076e5d82e0e3d526c5683a9b
Christina Vekri <christina.vek@gmail.com>
2024-01-15 13:06:13 +0100

    css for meme p

--- src/views/MemeImage.jsx
+++ src/views/MemeImage.jsx
@@ -1,54 +1,46 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const MemeImage = () => {
-  const [memeImages, setMemeImage] = useState([])
-  const [memeIndex, setMemeIndex] = useState(0)
-  const [loading, setLoading] = useState(true)
-
+  const [memeImages, setMemeImage] = useState([]);
+  const [memeIndex, setMemeIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const getImages = async () => {
     try {
       setLoading(true);
-      const response = await fetch('https://api.imgflip.com/get_memes')
-      const data = await response.json()
-      setMemeImage(data.data.memes)
-      setLoading(false)
-      
+      const response = await fetch('https://api.imgflip.com/get_memes');
+      const data = await response.json();
+      setMemeImage(data.data.memes);
+      setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.error(error);
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     getImages();
-  }, [])
-
-
+  }, []);
 
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : 
-
-
-      <div>
-        <img src={memeImages[memeIndex].url} alt="meme" /> <br />
-        <input type="text" placeholder='Type meme text for top '/>
-        <input type="text" placeholder='Type meme text for bottom '/>
-        {memeIndex>0 && <button onClick={() =>setMemeIndex(memeIndex-1)}>Previous Meme</button>}
-        <button onClick={() =>setMemeIndex(memeIndex+1)}>Next Meme</button>
-      </div>
-      
-      }
-
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <div className='container'>
+            <img src={memeImages[memeIndex].url} alt="meme" /> <br />
+            <p className='topLeft'>Top Left</p>
+            <p className='bottom'>Bottom</p>
+          </div>
+          <input type="text" placeholder='Type meme text for top' />
+          <input type="text" placeholder='Type meme text for bottom' />
+          {memeIndex > 0 && <button onClick={() => setMemeIndex(memeIndex - 1)}>Previous Meme</button>}
+          <button onClick={() => setMemeIndex(memeIndex + 1)}>Next Meme</button>
+        </div>
+      )}
     </div>
+  );
+};
 
-
-    
-
-
-  )
-}
-
-export default MemeImage
+export default MemeImage;
