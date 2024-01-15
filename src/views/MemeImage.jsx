c3c0e8b1ff18f84883cdab1884fc8c63cfaad898
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-01-15 11:45:09 +0100

    added the logic for adding the input fields

--- src/views/MemeImage.jsx
+++ src/views/MemeImage.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const MemeImage = () => {
   const [memeImages, setMemeImage] = useState([])
   const [memeIndex, setMemeIndex] = useState(1)
   const [loading, setLoading] = useState(true)
-
+  const [topText, setTopText] = useState('')
+  const [bottomText, setBottomText] = useState('')
 
   const getImages = async () => {
     try {
-      setLoading(true);
+      setLoading(true)
       const response = await fetch('https://api.imgflip.com/get_memes')
       const data = await response.json()
       setMemeImage(data.data.memes)
       setLoading(false)
-      
     } catch (error) {
       console.log(error)
       setLoading(false)
@@ -22,30 +21,43 @@ const MemeImage = () => {
   }
 
   useEffect(() => {
-    getImages();
+    getImages()
   }, [])
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(topText, bottomText)
+    setTopText('')
+    setBottomText('')
 
+
+  }
 
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : 
-
-
-      <div>
-        <img src={memeImages[memeIndex].url} alt="meme" /> <br />
-        <input type="text" placeholder='Type meme text for top '/>
-        <input type="text" placeholder='Type meme text for bottom '/>
-      </div>
-      
-      }
-
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <img src={memeImages[memeIndex].url} alt="meme" /> <br />
+            <input
+              type="text"
+              placeholder='Type meme text for top'
+              value={topText}
+              onChange={(e) => setTopText(e.target.value)}
+            /><br />
+            <input
+              type="text"
+              placeholder='Type meme text for bottom'
+              value={bottomText}
+              onChange={(e) => setBottomText(e.target.value)}
+            /><br />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
     </div>
-
-
-    
-
-
   )
 }
 
